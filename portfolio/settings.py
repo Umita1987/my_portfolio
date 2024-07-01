79eb89264451bb1d5018e72a817541f23c6aa8fe
Tanya Shustova <umita1987@mail.ru>
2024-07-01 17:57:17 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#6@u&mj20n89#!qrugp$ak4c4@&*!d*wn)3=a_gxqic=4nr%jo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
